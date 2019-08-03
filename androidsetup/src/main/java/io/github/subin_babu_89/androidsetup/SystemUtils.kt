495a6adca30f10bcdf4b0fdb3bbf3247c7b32d1d
Subin Babu <subin.babu@gtl.net>
2019-08-02 17:54:01 -0700

    add better intent launchers

--- androidsetup/src/main/java/io/github/subin_babu_89/androidsetup/SystemUtils.kt
+++ androidsetup/src/main/java/io/github/subin_babu_89/androidsetup/SystemUtils.kt
@@ -1,7 +1,9 @@
 package io.github.subin_babu_89.androidsetup
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -25,6 +27,56 @@ fun <T : AppCompatActivity> KClass<T>.start(
     if (finish) activity.finish()
 }
 
-fun showToast(context: Context,message:String){
+fun showToast(context: Context, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
+
+
+/**
+ *
+ * // Simple Activities
+launchActivity<UserDetailActivity>()
+
+// Add Intent extras
+launchActivity<UserDetailActivity> {
+putExtra(INTENT_USER_ID, user.id)
+}
+
+// Add custom flags
+launchActivity<UserDetailActivity> {
+putExtra(INTENT_USER_ID, user.id)
+addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+}
+
+// Add Shared Transistions
+val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, avatar, "avatar")
+launchActivity<UserDetailActivity>(options = options) {
+putExtra(INTENT_USER_ID, user.id)
+}
+
+// Add requestCode for startActivityForResult() call
+launchActivity<UserDetailActivity>(requestCode = 1234) {
+putExtra(INTENT_USER_ID, user.id)
+}
+ *
+ *
+ */
+inline fun <reified T : Any> Activity.launchActivity(
+    requestCode: Int = -1,
+    options: Bundle? = null,
+    noinline init: Intent.() -> Unit = {}
+) {
+    val intent = newIntent<T>(this)
+    intent.init()
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) startActivityForResult(intent, requestCode, options)
+    else startActivityForResult(intent, requestCode)
+}
+
+inline fun <reified T : Any> Context.launchActivity(options: Bundle? = null, noinline init: Intent.() -> Unit = {}) {
+    val intent = newIntent<T>(this)
+    intent.init()
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) startActivity(intent, options)
+    else startActivity(intent)
+}
+
+inline fun <reified T : Any> newIntent(context: Context): Intent = Intent(context, T::class.java)
